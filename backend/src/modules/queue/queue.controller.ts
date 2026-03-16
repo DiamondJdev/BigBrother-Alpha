5c416f184602a8a0b1cb79fd494178aee5998860
diamondjdev <cameronginther@gmail.com>
2026-03-16 08:16:43 -0500

    fix: fix queue.controller.ts to use DTO's for validation

--- backend/src/modules/queue/queue.controller.ts
+++ backend/src/modules/queue/queue.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { QueueService } from './queue.service';
+import { QueueStatusDto } from './dto/queueStatus.dto';
+import { enqueueJobDto } from './dto/enqueueJob.dto';
+import { BodyRequiredGuard } from '../auth/guard/body-required.guard';
 
 @Controller('queue')
+@UseGuards(JwtAuthGuard)
 export class QueueController {
   constructor(private readonly queueService: QueueService) {}
 
   @Post('enqueue')
-  async enqueue(@Body() body: { queueName: string; job: any }) {
+  @UseGuards(BodyRequiredGuard)
+  async enqueue(@Body() body: enqueueJobDto) {
     await this.queueService.enqueue(body.queueName, body.job);
     return { message: 'Job enqueued' };
   }
 
   @Post('status')
-  async status(@Body() body: { queueName: string }) {
+  @UseGuards(BodyRequiredGuard)
+  async status(@Body() body: QueueStatusDto) {
     const length = await this.queueService.getQueueLength(body.queueName);
     return { queueLength: length };
   }
